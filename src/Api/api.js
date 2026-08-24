1ea1684890f3f3f35348e857c1b4d2ca4b27a6c1
amal vb <amal@aventusinformatics.com>
2026-08-24 12:08:52 +0530

    chore: remove intelgenz-uat-frontend project files

--- src/Api/api.js
+++
@@ -1,5 +0,0 @@
-//threat-card
-export const GET_THREAT_CARD = '/Threat-card'
-export const CHAT_WS_ENDPOINT = '/ws'
-export const CHAT_HTTP_ENDPOINT = '/chat'
-export const CHAT_HEALTH_ENDPOINT = '/health'
