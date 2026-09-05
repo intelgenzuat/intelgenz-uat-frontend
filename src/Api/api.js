25e4052c6422b604ba5a1826bfb2e9aac39ed4c4
amal vb <amal@aventusinformatics.com>
2026-09-05 19:20:03 +0530

    feat: add Malware TTP view page with search functionality and API integration

--- src/Api/api.js
+++ src/Api/api.js
@@ -3,3 +3,12 @@ export const GET_THREAT_CARD = '/Threat-card'
 export const CHAT_WS_ENDPOINT = '/ws'
 export const CHAT_HTTP_ENDPOINT = '/chat'
 export const CHAT_HEALTH_ENDPOINT = '/health'
+
+//test 
+export const TEST = '/api/v1/health'
+
+//malware-ttp
+export const GET_MALWARE_TTP = '/api/v1/threat-ttps/malware/'
+
+//defend
+export const POST_DEFEND = '/api/v1/threat-ttps/malware/mapping'
