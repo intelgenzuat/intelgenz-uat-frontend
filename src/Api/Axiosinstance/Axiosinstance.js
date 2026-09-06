775633c7c783138f62206c99de301b918b421bb7
amal vb <amal@aventusinformatics.com>
2026-09-06 19:26:50 +0530

    feat: create axios instance with base URL configured from environment variables

--- src/Api/Axiosinstance/Axiosinstance.js
+++ src/Api/Axiosinstance/Axiosinstance.js
@@ -3,6 +3,9 @@ import axios from "axios"
 const apiUrl = import.meta.env.VITE_API_URL;
 let headers = {};
 
+console.log(".env",apiUrl);
+
+
 const axiosInstance = axios.create({
     baseURL: apiUrl,
     headers
