94bf219b975aff715ba240fe196fa612347b91e6
amal vb <amal@aventusinformatics.com>
2026-09-23 14:35:24 +0530

    feat: add UI components and styles for threat actor profiling, knowledge graphs, and mitigation TTP views

--- src/pages/ThreatActorProfiling/ThreatActorProfilingTable.jsx
+++ src/pages/ThreatActorProfiling/ThreatActorProfilingTable.jsx
@@ -7,6 +7,7 @@ import cube from '../../assets/images/cube.png';
 import logo from '../../assets/images/logo.jpeg';
 import '../../assets/styles/threatactorprofile/threatactorprofilimg.scss';
 import { getThreatActorProfiling, getThreatActorByTechniques, getThreatActorProfilingtable } from '../../Context/ThreatActorprofiling';
+import AdversaryTriageTopcontent from './AdversaryTriageTopcontent';
 
 export default function ThreatActorProfilingTable() {
   const navigate = useNavigate();
@@ -365,31 +366,7 @@ export default function ThreatActorProfilingTable() {
   return (
     <div className="threat-actor-detail-page">
       {/* Top Header Section */}
-      <div className="tap-header-section">
-        {/* Background Image/Abstract Graphic */}
-        {/* <div className="tap-hero-bg d-none d-md-block" style={{ backgroundImage: `url(${cube})` }}>
-        </div> */}
-
-        <div className="tap-header-content">
-          {/* Breadcrumbs */}
-          <div className="breadcrumb-nav text-muted" style={{ fontSize: '14px', marginBottom: '16px' }}>
-            <FiHome className="home-icon me-2" />
-            <span onClick={() => navigate('/')} style={{ cursor: 'pointer' }}>Home</span>
-            <span className="mx-2 text-black-50">/</span>
-            <span className="text-dark fw-medium">Threat Actor Profile</span>
-          </div>
-
-          {/* Title Row */}
-          <div className="tap-title-row">
-            <div className="shield-icon-wrapper">
-              <svg width="18" height="20" viewBox="0 0 19 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M9.43627 16.4528C9.72418 16.4528 9.96554 16.3555 10.1604 16.1609C10.3552 15.9661 10.4526 15.7247 10.4526 15.4368C10.4526 15.1489 10.3552 14.9075 10.1604 14.7127C9.96554 14.5179 9.72418 14.4205 9.43627 14.4205C9.14836 14.4205 8.907 14.5179 8.7122 14.7127C8.51739 14.9075 8.41999 15.1489 8.41999 15.4368C8.41999 15.7247 8.51739 15.9661 8.7122 16.1609C8.907 16.3555 9.14836 16.4528 9.43627 16.4528ZM8.49265 12.1703H10.3799V6.09741H8.49265V12.1703ZM9.43627 23.8326C6.71506 23.0905 4.4621 21.4887 2.67739 19.0273C0.892462 16.5659 0 13.8141 0 10.7718V3.53263L9.43627 0L18.8725 3.53263V10.7718C18.8725 13.8141 17.9801 16.5659 16.1952 19.0273C14.4104 21.4887 12.1575 23.0905 9.43627 23.8326Z" fill="#E9004A" />
-              </svg>
-            </div>
-            <h4>Adversary Triage</h4>
-          </div>
-        </div>
-      </div>
+      <AdversaryTriageTopcontent />
 
       {/* View Controls Card */}
       <div className="view-controls-card flex-shrink-0 mx-4 mb-4">
